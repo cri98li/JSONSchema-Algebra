66b1768ef5ab40a18e8ce117b52f8aa56c69fab3
cri98li <cri98li@gmail.com>
2020-03-02 22:25:40 +0100

    settato Maven (si spera)

---
+++ Programmi/JsonSchema_to_Algebra/src/main/java/it/unipi/di/tesiFalleniLandi/JsonSchema_to_Algebra/App.java
@@ -0,0 +1,13 @@
+package it.unipi.di.tesiFalleniLandi.JsonSchema_to_Algebra;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello Worldissimo!" );
+    }
+}
