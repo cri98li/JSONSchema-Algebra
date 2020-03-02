72029d31b26a0d44f20582e6db64ac9426692acf
cri98li <cri98li@gmail.com>
2020-03-02 22:20:44 +0100

    Settato maven

---
+++ Programmi/JsonSchema_to_Algebra/src/test/java/it/unipi/di/tesiFalleniLandi/JsonSchema_to_Algebra/AppTest.java
@@ -0,0 +1,20 @@
+package it.unipi.di.tesiFalleniLandi.JsonSchema_to_Algebra;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
