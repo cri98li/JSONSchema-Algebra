812e4246a04606c74e3fe62437bce8e9cb269d9d
Checco9811 <56653917+Checco9811@users.noreply.github.com>
2020-04-15 19:39:14 +0200

    Add main json

--- Programmi/JsonSchema_to_Algebra/src/main/java/it/unipi/di/tesiFalleniLandi/JsonSchema_to_Algebra/MainClass.java
+++ Programmi/JsonSchema_to_Algebra/src/main/java/it/unipi/di/tesiFalleniLandi/JsonSchema_to_Algebra/MainClass.java
@@ -23,92 +23,107 @@ import it.unipi.di.tesiFalleniLandi.JsonSchema_to_Algebra.JSONSchema.JSONSchema;
 import it.unipi.di.tesiFalleniLandi.JsonSchema_to_Algebra.JSONSchema.Utils_JSONSchema;
 import it.unipi.di.tesiFalleniLandi.JsonSchema_to_Algebra.JSONSchema.MainClass_JSONSchema;
 
-public class MainClass 
+public class MainClass
 {
-    public static void main( String[] args ) throws FileNotFoundException, IOException, ParseException{	
-    	String filename = "";
-    	int op;
-    
-    	if(args.length != 0) {
-    		try {
-	    		Assertion schema;
-	    		op = Integer.parseInt(args[0]);
-	    		filename = args[1];
-	    		
-	    		try (Reader reader = new FileReader(filename)){
-	    	        GrammaticaLexer lexer = new GrammaticaLexer(CharStreams.fromReader(reader));        
-	    	        CommonTokenStream tokens = new CommonTokenStream(lexer);
-	    	        GrammaticaParser parser = new GrammaticaParser(tokens);
-	    	        
-	    	        ParseTree tree =  parser.assertion();
-	    	        AlgebraParser p = new AlgebraParser();
-	    	        schema = (Assertion) p.visit(tree);
-	            }
-	    		
-	    		switch(op) {
-	    		
-	    		//JSON --> ALGEBRA
-	    		case 1:
-	    			JSONObject json = (JSONObject)schema.toJSONSchema();
-	    			JSONSchema jsonSchema = new JSONSchema(json);
-	    			System.out.println(Utils_JSONSchema.toGrammarString(Utils_JSONSchema.normalize(jsonSchema)));
-	    			break;
-	    			
-	    		//ALGEBRA --> JSON
-	    		case 2:
-	    			JSONObject JSON = (JSONObject)schema.toJSONSchema();
-	    			System.out.println(JSON.toJSONString());
-	    			break;
-	    			
-	    		//NOT ELIMINATION
-	    		case 3:
-	    			System.out.println(Utils.beauty(schema.notElimination().toGrammarString()));
-	    			break;
-	    			
-	    		default:
-	    			System.out.println("Not supported");
-	    			break;
-	    		}
-	    		
-    		}catch(Exception ex) {
-    			ex.printStackTrace();
-    			System.exit(-1);
-    		}
-    		
-    	}else {
-	    	System.out.println("[1] MainClass_JSONSchema");
-	    	System.out.println("[2] MainClass_Algebra");
-	    	System.out.println("[3] JSONSchema --> Algebra");
-	    	
-	    	try (Scanner s = new Scanner(System.in)) {
+	public static void main( String[] args ) throws FileNotFoundException, IOException, ParseException{
+		String filename = "";
+		int op;
+
+		if(args.length != 0) {
+			try {
+				op = Integer.parseInt(args[0]);
+				filename = args[1];
+
+				switch(op) {
+
+					//JSON --> ALGEBRA
+					case 1:
+						JSONSchema JsonSchema;
+						try (Reader reader = new FileReader(filename)) {
+							JSONObject object = (JSONObject) new JSONParser().parse(reader);
+							JsonSchema = new JSONSchema(object);
+						}
+
+						System.out.println(Utils_JSONSchema.toGrammarString(Utils_JSONSchema.normalize(JsonSchema)));
+						break;
+
+					//ALGEBRA --> JSON
+					case 2:
+						Assertion schema;
+						try (Reader reader = new FileReader(filename)){
+							GrammaticaLexer lexer = new GrammaticaLexer(CharStreams.fromReader(reader));
+							CommonTokenStream tokens = new CommonTokenStream(lexer);
+							GrammaticaParser parser = new GrammaticaParser(tokens);
+
+							ParseTree tree =  parser.assertion();
+							AlgebraParser p = new AlgebraParser();
+							schema = (Assertion) p.visit(tree);
+						}
+
+						JSONObject JSON = (JSONObject)schema.toJSONSchema();
+						System.out.println(JSON.toJSONString());
+						break;
+
+					//NOT ELIMINATION
+					case 3:
+						Assertion _schema;
+						try (Reader reader = new FileReader(filename)){
+							GrammaticaLexer lexer = new GrammaticaLexer(CharStreams.fromReader(reader));
+							CommonTokenStream tokens = new CommonTokenStream(lexer);
+							GrammaticaParser parser = new GrammaticaParser(tokens);
+
+							ParseTree tree =  parser.assertion();
+							AlgebraParser p = new AlgebraParser();
+							_schema = (Assertion) p.visit(tree);
+						}
+
+						System.out.println(Utils.beauty(_schema.notElimination().toGrammarString()));
+						break;
+
+					default:
+						System.out.println("Not supported");
+						break;
+				}
+
+			}catch(Exception ex) {
+				ex.printStackTrace();
+				System.exit(-1);
+			}
+
+		}else {
+			System.out.println("[1] MainClass_JSONSchema");
+			System.out.println("[2] MainClass_Algebra");
+			System.out.println("[3] JSONSchema --> Algebra");
+
+			try (Scanner s = new Scanner(System.in)) {
 				switch(s.nextInt()) {
-				case 1:
-					MainClass_JSONSchema.main(args);
-					break;
-				case 2:
-					MainClass_Algebra.main(args);
-					break;
-				case 3:
-					String path = "test.json";
-					JSONSchema root;
-					
-				    try (Reader reader = new FileReader(path)){
-				    	JSONObject object = (JSONObject) new JSONParser().parse(reader);
-				    	root = new JSONSchema(object);
-				    	
-				    	Utils_JSONSchema.toGrammarString(root.assertionSeparation());
-				    	
-				    	GrammaticaLexer lexer = new GrammaticaLexer(CharStreams.fromString(Utils_JSONSchema.toGrammarString(root.assertionSeparation())));        
-				        CommonTokenStream tokens = new CommonTokenStream(lexer);
-				        GrammaticaParser parser = new GrammaticaParser(tokens);
-				        ParseTree tree =  parser.assertion();
-				        AlgebraParser p = new AlgebraParser();
-				        Assertion schema = (Assertion) p.visit(tree);
-				        System.out.println(schema.toString());
-				    }
-					break;
+					case 1:
+						MainClass_JSONSchema.main(args);
+						break;
+					case 2:
+						MainClass_Algebra.main(args);
+						break;
+					case 3:
+						String path = "test.json";
+						JSONSchema root;
+
+						try (Reader reader = new FileReader(path)){
+							JSONObject object = (JSONObject) new JSONParser().parse(reader);
+							root = new JSONSchema(object);
+
+							Utils_JSONSchema.toGrammarString(root.assertionSeparation());
+
+							GrammaticaLexer lexer = new GrammaticaLexer(CharStreams.fromString(Utils_JSONSchema.toGrammarString(root.assertionSeparation())));
+							CommonTokenStream tokens = new CommonTokenStream(lexer);
+							GrammaticaParser parser = new GrammaticaParser(tokens);
+							ParseTree tree =  parser.assertion();
+							AlgebraParser p = new AlgebraParser();
+							Assertion schema = (Assertion) p.visit(tree);
+							System.out.println(schema.toString());
+						}
+						break;
 				}
 			}
-    	}
-    }
+		}
+	}
 }
